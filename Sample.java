3b91d867d57d7debc847f7364c94c861c7c330e5
Xuanming Zhang <99805021+XMZhangAI@users.noreply.github.com>
2024-11-07 22:49:17 +0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,20 +1,19 @@
-/*** Fine-grained Reminding Prompting
-@param Pay attention to JSONException
+/*** Fine-grained Inspiring Prompting
+@param Pay attention to JSONException. If the JSON data is malformed or not in the expected format, it raises an exception.
 */
-public void load(String json) { 
+public void load(String json) {
   mChanged = false;
-  mModels = new HashMap<Long, JSONObject>(); 
+  mModels = new HashMap<Long, JSONObject>();
   try {
-    JSONObject modelarray = new JSONObject(json); 
+    JSONObject modelarray = new JSONObject(json);
     JSONArray ids = modelarray.names();
     if (ids != null) {
       for (int i = 0; i < ids.length(); i++) {
         String id = ids.getString(i);
-        JSONObject o = modelarray.getJSONObject(id); 
+        JSONObject o = modelarray.getJSONObject(id);
         mModels.put(o.getLong("id"), o);
-      }  
+      }
     }
   } catch (JSONException e) {
-    throw new RuntimeException(e);
   }
 }
