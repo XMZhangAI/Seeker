6a67bc32be16e27bb87c02d7a510711a83ba4207
Xuanming Zhang <99805021+XMZhangAI@users.noreply.github.com>
2024-11-07 22:45:56 +0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,20 +1,20 @@
-/*** Coarse-grained Reminding Prompting
-@param Pay attention to potential exceptions
+/*** Fine-grained Reminding Prompting
+@param Pay attention to JSONException
 */
-public void load(String json) { 
-  mChanged = false;
-  mModels = new HashMap<Long, JSONObject>(); 
+public void load(String json) {
   try {
-    JSONObject modelarray = new JSONObject(json); 
+    mChanged = false;
+    mModels = new HashMap<Long, JSONObject>();
+  
+    JSONObject modelarray = new JSONObject(json);
     JSONArray ids = modelarray.names();
     if (ids != null) {
       for (int i = 0; i < ids.length(); i++) {
         String id = ids.getString(i);
-        JSONObject o = modelarray.getJSONObject(id); 
+        JSONObject o = modelarray.getJSONObject(id);
         mModels.put(o.getLong("id"), o);
-      }  
+      }
     }
   } catch (JSONException e) {
-    throw new RuntimeException(e);
   }
 }
