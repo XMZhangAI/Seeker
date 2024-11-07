00026708f839e8507a837934e00e0405e4600d29
Xuanming Zhang <99805021+XMZhangAI@users.noreply.github.com>
2024-11-07 21:05:27 +0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,24 +1,19 @@
 /*** Coarse-grained Reminding Prompting
 @param Pay attention to potential exceptions
 */
-public void load(String json) {
-  try {
+public void load(String json) throws JSONException {
     mChanged = false;
     mModels = new HashMap<Long, JSONObject>();
+    try {
     JSONObeject modelarray = new JSONObeject(json);
     JSONArray ids = modelarray.names();
     if (ids != null) {
       for (int i = 0; i < ids.length(); i++) {
         String id = ids.getString(i);
-        JSONObject o = modelarray.getJSONObject(id);
+        JSONObject o = modelArray.getJSONObject(id);
         mModels.put(o.getLong("id"), o);
       }
     }
   } catch (JSONException e) {
-    System.err.println("JSON processing error: " + e.getMessage());
-  } catch (NullPointerException e) {
-    System.err.println("Null value encountered: " + e.getMessage());
-  } catch (Exception e) {
-    System.err.println("Unexpected error: " + e.getMessage());
-  }
-}
+    throw new JSONException("Error processing JSON data: " + e.getMessage());
+  } 
