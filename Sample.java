4d610ea2249d769dc16bd0a5fbd873e4e30458b4
Xuanming Zhang <99805021+XMZhangAI@users.noreply.github.com>
2024-11-07 21:57:39 +0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,27 +1,20 @@
 /*** Fine-grained Reminding Prompting
 @param Pay attention to JSONException
 */
-public void load(String json) {
-  try {
+public void load(String json) throws JSONException {
     mChanged = false;
     mModels = new HashMap<Long, JSONObject>();
-    JSONObeject modelarray = new JSONObeject(json);
-    JSONArray ids = modelarray.names();
-    if (ids != null) {
-      for (int i = 0; i < ids.length(); i++) {
-        String id = ids.getString(i);
-        JSONObject o = modelArray.getJSONObject(id);
-        if (o.has("id")) {
+    try {
+      JSONObject modelArray = new JSONObject(json);
+      JSONArray ids = modelArray.names();
+      if (ids != null) {
+        for (int i = 0; i < ids.length(); i++) {
+          String id = ids.getString(i);
+          JSONObject o = modelArray.getJSONObject(id);
           mModels.put(o.getLong("id"), o);
-        } else {
-          System.err.println("JSONObject for ID " + id + " does not contain 'id' field.");
         }
+      }
+    } catch (JSONException e) {
+      throw new JSONException("Error processing JSON data: " + e.getMessage());
     }
-  } catch (JSONException e) {
-    System.err.println("JSON processing error: " + e.getMessage());
-  } catch (NullPointerException e) {
-    System.err.println("Null value encountered: " + e.getMessage());
-  } catch (Exception e) {
-    System.err.println("Unexpected error: " + e.getMessage());
-  }
 }
