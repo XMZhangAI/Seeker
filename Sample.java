2c87856a8948ffd794602a9aa56e43bd46882f5b
Xuanming Zhang <99805021+XMZhangAI@users.noreply.github.com>
2024-11-07 22:40:42 +0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,16 +2,19 @@
 @param Pay attention to potential exceptions
 */
 public void load(String json) { 
+  mChanged = false;
+  mModels = new HashMap<Long, JSONObject>(); 
   try {
-    mChanged = false;
-    mModels = new HashMap<Long, JSONObject>();
-      JSONObject modelarray = ne JSONObject(json); 
-      JSONArray ids = modelarray.names();
-      if (ids != null) {
-        for (int i = 0; i < ids.length(); i++) {
-          String id = ids.getString(i);
-          JSONObject o = modelarray.getJSONObject(id);
-          mModels.put(o.getLong("id"), o); 
-        } catch (Exception e) {
-        }
+    JSONObject modelarray = new JSONObject(json); 
+    JSONArray ids = modelarray.names();
+    if (ids != null) {
+      for (int i = 0; i < ids.length(); i++) {
+        String id = ids.getString(i);
+        JSONObject o = modelarray.getJSONObject(id); 
+        mModels.put(o.getLong("id"), o);
+      }  
+    }
+  } catch (JSONException e) {
+    throw new RuntimeException(e);
+  }
 }
