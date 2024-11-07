e67f6a5cdefbeeef94b6129badcb44e40bfd13b5
Xuanming Zhang <99805021+XMZhangAI@users.noreply.github.com>
2024-11-07 22:36:00 +0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,20 +1,17 @@
-/*** Fine-grained Guiding Prompting
-@param Pay attention to the JSONException. If you observe which lines of code are prone to the possibility of incorrect JSON data format or not the expected format, try the possible lines together to return JSONException, and do not arbitarily add code to affect the original function.
+/*** Coarse-grained Reminding Prompting
+@param Pay attention to potential exceptions
 */
-public void load(String json) throws JSONException {
+public void load(String json) { 
+  try {
     mChanged = false;
     mModels = new HashMap<Long, JSONObject>();
-    try {
-      JSONObject modelArray = new JSONObject(json);
-      JSONArray ids = modelArray.names();
+      JSONObject modelarray = ne JSONObject(json); 
+      JSONArray ids = modelarray.names();
       if (ids != null) {
         for (int i = 0; i < ids.length(); i++) {
           String id = ids.getString(i);
-          JSONObject o = modelArray.getJSONObject(id);
-          mModels.put(o.getLong("id"), o);
+          JSONObject o = modelarray.getJSONObject(id);
+          mModels.put(o.getLong("id"), o); 
+        } catch (Exception e) {
         }
-      }
-    } catch (JSONException e) {
-      throw new JSONException("Error processing JSON data: " + e.getMessage());
-    }
 }
