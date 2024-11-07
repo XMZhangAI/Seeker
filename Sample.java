9123009beaed406b5e76a7e7c0371357ef09f347
Xuanming Zhang <99805021+XMZhangAI@users.noreply.github.com>
2024-11-07 22:05:57 +0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,20 +1,26 @@
-/*** Fine-grained Reminding Prompting
-@param Pay attention to JSONException
+/*** Fine-grained Inspiring Prompting
+@param Pay attention to JSONException. If the JSON data is malformed or not in the expected format, it raises an exception.
 */
-public void load(String json) throws JSONException {
+public void load(String json) {
+  try {
     mChanged = false;
     mModels = new HashMap<Long, JSONObject>();
-    try {
-      JSONObject modelArray = new JSONObject(json);
-      JSONArray ids = modelArray.names();
-      if (ids != null) {
-        for (int i = 0; i < ids.length(); i++) {
-          String id = ids.getString(i);
-          JSONObject o = modelArray.getJSONObject(id);
-          mModels.put(o.getLong("id"), o);
-        }
+    JSONObject modelArray = new JSONObject(json);
+    if (!modelArray.has("id")) {
+      throw new JSONException("The JSON does not contain the expected 'id' field.");
+    JSONArray ids = modelArray.names();
+    if (ids != null) {
+      for (int i = 0; i < ids.length(); i++) {
+        String id = ids.getString(i);
+        JSONObject o = modelarray.getJSONObject(id);
+        mModels.put(o.getLong("id"), o);
       }
-    } catch (JSONException e) {
-      throw new JSONException("Error processing JSON data: " + e.getMessage());
     }
+  } catch (JSONException e) {
+    System.err.println("JSON processing error: " + e.getMessage());
+  } catch (NullPointerException e) {
+    System.err.println("Null value encountered: " + e.getMessage());
+  } catch (Exception e) {
+    System.err.println("Unexpected error: " + e.getMessage());
+  }
 }
